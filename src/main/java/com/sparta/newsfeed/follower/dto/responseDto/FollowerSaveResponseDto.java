b2796c434d4072e47091a01f4990f1f66d9a03b5
wjdtmdgjs-1 <jeong5207@naver.com>
2024-09-05 03:36:47 +0900

    완성

---
+++ src/main/java/com/sparta/newsfeed/follower/dto/responseDto/FollowerSaveResponseDto.java
@@ -0,0 +1,15 @@
+package com.sparta.newsfeed.follower.dto.responseDto;
+
+import com.sparta.newsfeed.follower.entity.Follower;
+import lombok.Getter;
+
+@Getter
+public class FollowerSaveResponseDto {
+    private final String followerEmail;
+    private final String userEmail;
+
+    public FollowerSaveResponseDto(Follower saveFollower) {
+        this.followerEmail = saveFollower.getFollower().getEmail();
+        this.userEmail = saveFollower.getUser().getEmail();
+    }
+}
---
+++ src/main/java/com/sparta/newsfeed/follower/repository/FollowerRepository.java
@@ -0,0 +1,20 @@
+package com.sparta.newsfeed.follower.repository;
+
+import com.sparta.newsfeed.follower.entity.Follower;
+import com.sparta.newsfeed.profile.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface FollowerRepository extends JpaRepository<Follower, Long> {
+    Optional<User> findByEmail(String email);
+
+    Follower findByUserAndFollower(User user, User follower);
+
+    Follower findByUserIdAndFollowerId(Long userId, Long followerId);
+
+    List<Follower> findByFollower(User user);
+
+    List<Follower> findByUser(User user);
+}
